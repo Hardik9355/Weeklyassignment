864c88fcf552474f1dd47249ffa15f6217bc64b6
Hardik9355 <110618175+Hardik9355@users.noreply.github.com>
2023-01-31 00:31:07 -0500

    Update fibonnaci.js

--- fibonnaci.js
+++ fibonnaci.js
@@ -7,14 +7,3 @@ for (let i =1; i<=n;i++){
     b = c;  
 }
 
-const number = 5;
-let n1 = 0, n2 = 1, nextTerm;
-
-console.log('Fibonacci Series:');
-
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
-}
